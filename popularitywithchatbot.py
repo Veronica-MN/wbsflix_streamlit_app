14a5f5a70a7766a56c54dbd9e7243d1beefeb933
Veronica Nyamangara <42588025+Veronica-MN@users.noreply.github.com>
2023-11-09 18:29:22 +0100

    Add files via upload

--- popularitywithchatbot.py
+++ popularitywithchatbot.py
@@ -39,27 +39,4 @@ genres = {'1': 'Comedy', '2': 'Drama', '3': 'Thriller'}
 st.title("Personal Recommender")
 #st.chat_message("assistant", "Hi! I'm your personal recommender Human!!!")
 
-while True:
-    st.chat_message("assistant", "Please choose a movie genre:")
-    genre_choice = st.text_input("Choose, 1 = Comedy, 2 = Drama, 3 = Thriller")
 
-    if genre_choice not in genres:
-        st.chat_message("assistant", "Invalid genre selection. Please choose 1, 2, or 3.")
-    else:
-        selected_genre = genres[genre_choice]
-        st.chat_message("user", f"You have chosen {selected_genre}")
-
-        recommended_movies = popularity_recommender(10)
-
-        a = recommended_movies[recommended_movies["genres"].str.contains(selected_genre)]
-
-        if not a.empty:
-            st.chat_message("assistant", "Here are your personal movie recommendations, Enjoy!:")
-            for movie_title in a['title']:
-                st.chat_message("assistant", movie_title)
-        else:
-            st.chat_message("assistant", "No recommendations found now, sorry.")
-
-    continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
-    if continue_chat == "No":
-        break
