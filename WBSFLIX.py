43d241242ca1e32c2b198833af378a211a78f9ae
Veronica Nyamangara <42588025+Veronica-MN@users.noreply.github.com>
2023-11-09 16:50:30 +0100

    Add files via upload

--- WBSFLIX.py
+++ WBSFLIX.py
@@ -2,64 +2,16 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-
-
-uploaded_file = st.file_uploader("top_movies.csv")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe) 
-
-
+genres = ['Comedy', 'Drama', 'Thriller']
 
 st.title("Personal Recommender")
 st.write("Hi! I'm your personal recommender.")
-
-@st.cache_data
-uploaded_files = st.file_uploader("top_movies.csv", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("top_movies:", uploaded_file.name)
-    st.write(bytes_data)
-
-
-def popularity_based_recommender(n):
-  movie_info_columns = ['movieId', 'title', 'genres']
-  rating_count_df = top_movies.groupby('movieId')['rating'].agg(['mean', 'count']).reset_index()
-  from sklearn.preprocessing import MinMaxScaler
-  scaler = MinMaxScaler(feature_range=(1, 5))
-  rating_count_df['scaled_count'] = scaler.fit_transform(rating_count_df[['count']])
-  rating_count_df['rank'] = (rating_count_df['mean'])+rating_count_df['scaled_count']
-
-  df= (
-   rating_count_df
-   .drop_duplicates(subset='movieId')
-   .merge(top_movies.drop_duplicates(subset='movieId'),
-       on='movieId',
-       how='left')
-    [movie_info_columns + ["mean", "count", "rank"]]
-    .nlargest(10, 'rank')
-     )
-
-  return df.head(10)
 
 genre = st.radio("Choose your genre:", genres)
 
 if genre:
     st.write(f"You have chosen {genre}")
-    df = popularity_based_recommender(15) 
+    df = popularity_based_recommender(15)
     if genre == 'Comedy':
         a = df[df["genres"].str.contains("Comedy")]
     elif genre == 'Drama':
@@ -74,6 +26,45 @@ if genre:
         st.table(a[['title']].reset_index(drop=True))
     else:
         st.write("No recommendations found for this genre.")
+
+# FOR CHAT BOT !!!
+
+import streamlit as st
+import pandas as pd
+from your_popularity_based_recommender_module import popularity_based_recommender
+
+
+genres = {'1': 'Comedy', '2': 'Drama', '3': 'Thriller'}
+
+st.title("Personal Recommender")
+st.chat_message("assistant", "Hi! I'm your personal recommender Human!!!")
+
+while True:
+    st.chat_message("assistant", "Please choose a movie genre:")
+    genre_choice = st.text_input("Choose, 1 = Comedy, 2 = Drama, 3 = Thriller")
+
+    if genre_choice not in genres:
+        st.chat_message("assistant", "Invalid genre selection. Please choose 1, 2, or 3.")
+    else:
+        selected_genre = genres[genre_choice]
+        st.chat_message("user", f"You have chosen {selected_genre}")
+        df = popularity_based_recommender(10) # (DF) NEEDS TO BE CHANGE HEREEEEEEEE
+
+
+        a = df[df["genres"].str.contains(selected_genre)] # < a > FILTERS FOR THE CHOSSEN GENRE
+
+        if not a.empty:
+            st.chat_message("assistant", "Here is your personal movie recomendations, Enjoy!:")
+            for movie_title in a['title']:
+                st.chat_message("assistant", movie_title)
+        else:
+            st.chat_message("assistant", "No recommendations found now, sorry.")
+
+    continue_chat = st.radio("Do you want another recomendation?", ["Yes", "No"])
+    if continue_chat == "No":
+        break
+
+  
 
 
 
