cdb25343ea3d1d4630e23142344abc7e93f205df
Veronica Nyamangara <42588025+Veronica-MN@users.noreply.github.com>
2023-11-10 10:51:35 +0100

    Update popularitywithchatbot_1.py

--- popularitywithchatbot_1.py
+++ popularitywithchatbot_1.py
@@ -1,46 +1,32 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 csv_url = "https://raw.githubusercontent.com/JesHP73/moviereco/85845a1166f211909985e551c8ab860ce58727b5/sample_movies.csv"
 
-
 def calculate_popularity(df):
-    
     ratings_count = df.groupby('movieId')['rating'].count()
-    
     average_ratings = df.groupby('movieId')['rating'].mean()
-    
     popularity_score = average_ratings * np.log1p(ratings_count)
-    
     return popularity_score.to_dict()
 
 def popularity_recommender(df, num_recommendations):
-    
     df['popularity_score'] = df['movieId'].map(calculate_popularity(df))
-    
     sorted_movies = df.sort_values(by='popularity_score', ascending=False)
-    
     recommended_movies = sorted_movies.drop_duplicates(subset='title').head(num_recommendations)
-
     return recommended_movies[['movieId', 'title', 'genres']]
 
 def run_streamlit_app():
-    
-    #df = pd.read_csv(csv_url) #df ok
-
     # Streamlit app starts from here for the user
     st.title("Personal Movie Recommender")
     st.write("Hi! I'm your personal recommender. 😊🎬")
-    
+
     genres = ['Comedy', 'Drama', 'Thriller']
     genre_choice = st.text_input("Choose your genre (type 1 for Comedy, 2 for Drama, 3 for Thriller):")
-    
+
     genre_map = {'1': 'Comedy', '2': 'Drama', '3': 'Thriller'}
 
-     # Use session state to create a unique cache key
+    # Use session state to create a unique cache key
     session_state = st.session_state
     if not hasattr(session_state, 'cache_key'):
         session_state.cache_key = 0
@@ -49,31 +35,29 @@ def run_streamlit_app():
         selected_genre = genre_map[genre_choice]
         st.write(f"You have chosen {selected_genre}")
 
-        #reload data
-        df = pd.read_csv(csv_url) #df ok
-        # recalculate recommendation with uniquq cache key
-        # top_movies = popularity_recommender(df, 15)
+        # Reload data
+        df = pd.read_csv(csv_url)
+        # Recalculate recommendation with unique cache key
         top_movies = st.cache(popularity_recommender)(df, selected_genre, 15, key=session_state.cache_key)
 
         if not top_movies.empty:
+            a = top_movies[top_movies["genres"].str.contains(selected_genre)]
 
-        a = top_movies[top_movies["genres"].str.contains(selected_genre)] 
+            if not a.empty:
+                st.write("Here are your personal movie recommendations, Enjoy! 🍿")
+                for movie_title in a['title']:
+                    st.write(movie_title)
+            else:
+                st.write("No recommendations found now, sorry. 😕")
 
-        if not a.empty:
-            st.write("Here are your personal movie recommendations, Enjoy! 🍿")
-            for movie_title in a['title']:
-                st.write(movie_title)
-        else:
-            st.write("No recommendations found now, sorry. 😕")
-
-        continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
-        if continue_chat == "No":
-            st.write("Thanks for using the recommender! Have a great day! 👋")
-            st.stop()
-        else:
-            # Increment the cache key to force recalculation on the next user input
-            session_state.cache_key += 1
-            st.write("Please type 1, 2, or 3 again. ⭐️")
+            continue_chat = st.radio("Do you want another recommendation?", ["Yes", "No"])
+            if continue_chat == "No":
+                st.write("Thanks for using the recommender! Have a great day! 👋")
+                st.stop()
+            else:
+                # Increment the cache key to force recalculation on the next user input
+                session_state.cache_key += 1
+                st.write("Please type 1, 2, or 3 again. ⭐️")
 
 if __name__ == "__main__":
     run_streamlit_app()
